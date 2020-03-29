0be307750d733dbab15890d338aebd785edbf627
David Baraff <nosuch@nowhere>
2020-03-28 20:04:39 -0700

    add type: .dynamic

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Debmate",
+            type: .dynamic,
             targets: ["Debmate", "DebmateC"]),
     ],
     dependencies: [

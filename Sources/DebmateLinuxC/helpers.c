059080c9ca311a1f22b4decfff27a836ca932384
David Baraff <nosuch@nowhere>
2023-03-27 10:45:43 -0700

    added linux_update_mtime

--- Sources/DebmateLinuxC/helpers.c
+++ Sources/DebmateLinuxC/helpers.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
+#include <utime.h>
 
 int64_t linux_file_size(const char *filename) {
     struct stat st;
@@ -17,8 +19,28 @@ int64_t linux_file_mtime(const char *filename) {
 
     if (stat(filename, &st) == 0) {
         return st.st_mtime;
+
     }
 
     return -1;
 }
 
+int linux_update_mtime(const char* filename, int olderThan) {
+    struct stat st;
+
+    if (stat(filename, &st) != 0) {
+        return 0;
+    }
+
+    int64_t m = st.st_mtime;
+    time_t now = time(NULL);
+
+    if (now - m < olderThan) {
+        return 1;
+    }
+
+    struct utimbuf new_times;
+    new_times.actime = st.st_atime;
+    new_times.modtime = now;
+    return utime(filename, &new_times) == 0;
+}
--- Sources/DebmateLinuxC/include/DebmateLinuxC.h
+++ Sources/DebmateLinuxC/include/DebmateLinuxC.h
@@ -1,6 +1,4 @@
 #include <stdint.h>
 int64_t linux_file_size(const char *filename);
 int64_t linux_file_mtime(const char *filename);
-
-
-
+int linux_update_mtime(const char* filename, int olderThan);

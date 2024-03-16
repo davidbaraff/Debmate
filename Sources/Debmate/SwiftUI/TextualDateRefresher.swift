3797b6f7dbff81332e1760bf689786e34e03966a
David Baraff <nosuch@nowhere>
2024-03-15 17:30:44 -0700

    added textualdaterefresher

---
+++ Sources/Debmate/SwiftUI/TextualDateRefresher.swift
@@ -0,0 +1,20 @@
+//
+//  TextualDateRefresher.swift
+//  Debmate
+//
+//  Created by David Baraff on 3/15/24.
+//
+
+import SwiftUI
+
+
+final public class TextualDateRefresher : ObservableObject {
+    private var timer: Debmate.Timer!
+
+    public init(refreshInterval seconds: Int) {
+        timer = Timer {
+            self.objectWillChange.send()
+        }
+        timer.start(repeating: Double(seconds))
+    }
+}
